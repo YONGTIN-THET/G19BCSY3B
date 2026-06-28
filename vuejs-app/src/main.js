d7f210e96f5bff3a5cfe00fd48a6a111c6e7e447
YONGTIN-THET <linsovannavong@gmail.com>
2026-06-28 23:14:35 +0700

    SESSION 3.1

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
+
+import './main.css'
+
 import App from './App.vue'
 import router from './router'
 
