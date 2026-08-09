8e046a25996e43e98bed4eb9724eea9618efd059
YONGTIN-THET <linsovannavong@gmail.com>
2026-08-09 23:30:36 +0700

    SESSION 4.4

--- vuejs-app/src/main.js
+++ vuejs-app/src/main.js
@@ -1,51 +1,58 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'admin-lte/dist/css/adminlte.min.css';
-
+import './main.css';git
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'admin-lte/dist/js/adminlte.min.js';
-
-import './main.css'; // ✅ បន្ថែមបន្ទាត់នេះ
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
 import { useUserStore } from '@/stores/user';
 import { apiVerify } from '@/functions/api/auth';
 
-// ... កូដដែលនៅសល់ដដែល
-
 const app = createApp(App)
-
 const pinia = createPinia();
+
 pinia.use(piniaPluginPersistedstate);
+
 app.use(pinia);
 app.use(router);
 app.mount('#app');
 
-
 const userStore = useUserStore();
-router.beforeEach(async (to, from) => {
-  const { guarded } = to.meta;
-  if (guarded === undefined) { // if the route is not guarded, we don't need to verify the token
-    return;
-  }
 
-  try {
+axios.interceptors.request.use((config) => {
     const token = userStore.getSanctumToken();
-    const response = await apiVerify(token);
-    const { data } = response;
-    userStore.setState(data.user);
-  } catch (error) {
-    userStore.reset();
-  }
 
-  if (guarded && !userStore.isAuthenticated) { // if the route is guarded and the user is not authenticated, redirect to signin page
-    return { name: 'auth.signin' };
-  }
-  if (!guarded && userStore.isAuthenticated) { // if the route is not guarded and the user is authenticated, redirect to dashboard page
-    return { name: 'dashboard' };
-  }
+    if (token && !config.headers.Authorization) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+router.beforeEach(async (to, from) => {
+    const { guarded } = to.meta;
+
+    if (guarded === undefined) {
+        return;
+    }
+
+    try {
+        const response = await apiVerify();
+        const { data } = response;
+
+        userStore.setState(data.user);
+    } catch (error) {
+        userStore.reset();
+    }
+
+    if (guarded && !userStore.isAuthenticated) {
+        return { name: 'auth.signin' };
+    }
+
+    if (!guarded && userStore.isAuthenticated) {
+        return { name: 'dashboard' };
+    }
 });
